f02d77e778077b36d30999b994b4daca144ad140
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-08-28 00:05:44 -0300

    clean

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const apiKey = 'a70dbfe19b800809dfdd3e89e8532c9e';
 
 input.onkeyup = function () {
   const q = input.value;
-  // const url = 'https://api.themoviedb.org/3/search/movie?api_key=a70dbfe19b800809dfdd3e89e8532c9e&query=' + q;
   const url = `https://api.themoviedb.org/3/search/movie?api_key=a70dbfe19b800809dfdd3e89e8532c9e&query=${q}`;
 
   fetch(url)
-    .then(res => /* return */ res.json())
+    .then(res => res.json())
     .then(data => {
       const movies = data.results;
 
@@ -16,12 +15,10 @@ input.onkeyup = function () {
       ul.style.display = 'block';
 
       document
-        .querySelectorAll('li.list-item') //devuelve una NodeList
+        .querySelectorAll('li.list-item') 
         .forEach(function (li) {
           li.addEventListener('click', function (e) {
-            //   const movieId = e.target.id;
-            const movieId = e.currentTarget.id;
-            
+            const movieId = e.currentTarget.id;           
 
             input.value = e.target.innerHTML;
             ul.style.display = 'none';
@@ -29,15 +26,10 @@ input.onkeyup = function () {
             fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`)
               .then(res => res.json())
               .then(data => {
-                console.log(data);
-
                 const elModal = document.querySelector('#el-modal');
-
-                // elModal.style.display = 'block';
 
                 document.querySelector('#el-modal .title').innerHTML = data.original_title;
                 document.querySelector('#el-modal .overview').innerHTML = data.overview;
-
 
                 elModal.classList.add('active');
               })
